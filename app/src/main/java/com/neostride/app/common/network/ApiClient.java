61f2fff4c6a188bd84835fa2732ffd13865177b6
C0deHater <32183897@dankook.ac.kr>
2026-04-28 22:52:22 +0900

    feat: apiclient.java base_url부분 입력 공간 GradleScript/local.properties로 이동

--- app/src/main/java/com/neostride/app/common/network/ApiClient.java
+++ app/src/main/java/com/neostride/app/common/network/ApiClient.java
@@ -1,5 +1,6 @@
 package com.neostride.app.common.network;
 
+import com.neostride.app.BuildConfig;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import retrofit2.Retrofit;
@@ -9,8 +10,8 @@ public class ApiClient {
 
     // 추후 팀장님 백엔드 담당 해주는 본체에 해당하는 ip 주소 넣기
     // ex) private static final String BASE_URL = "http://10.0.2.2:8080/";
-    // 현재 postman으로 신호가 가는지 확인 가능
-    private static final String BASE_URL = "https://6b068f2e-91de-4c62-be70-f8302ba5e407.mock.pstmn.io/";
+    // 추후 필요시 Gradle Scripts/local.properties 에 URL 변경하기
+    private static final String BASE_URL = BuildConfig.BASE_URL;
     private static Retrofit retrofit = null;
 
     public static Retrofit getInstance() {
